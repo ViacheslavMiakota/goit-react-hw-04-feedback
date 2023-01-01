599f0b73b6e7e4e85ac972959068267d81cf4570
ViacheslavMiakota <parazyt2010@gmail.com>
2023-01-01 17:59:28 +0200

    add protypes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,6 +4,7 @@ import Statistics from 'components/Statistics/Statistics';
 import { Container } from 'components/App/App.styled';
 import Section from 'components/Section/Section';
 import { ButtonBox } from 'components/Section/Section.styled';
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -13,6 +14,7 @@ const App = () => {
   const optionsKeys = { good, neutral, bad };
 
   const handleFeedback = name => {
+    console.log(name);
     switch (name) {
       case 'good':
         setGood(prevState => prevState + 1);
@@ -62,60 +64,3 @@ const App = () => {
   );
 };
 export default App;
-
-// class App extends React.Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-//   handleFeedback = option => {
-//     this.setState(prevState => ({
-//       [option]: prevState[option] + 1,
-//     }));
-//   };
-
-//   countTotalFeedback = () => {
-//     const values = Object.values(this.state);
-//     return values.reduce((acc, value) => acc + value, 0);
-//   };
-
-//   countPositiveFeedbackPercentage = (total, good) => {
-//     const positive = Math.round((good / total) * 100);
-//     return positive;
-//   };
-//   render() {
-//     const { good, neutral, bad } = this.state;
-//     const total = this.countTotalFeedback();
-//     const positivePercentage = this.countPositiveFeedbackPercentage(
-//       total,
-//       good
-//     );
-
-//     const options = Object.keys(this.state);
-
-//     return (
-//       <Container>
-//         <Section title="Please leave feedback">
-//           <ButtonBox>
-//             <FeedbackOptions
-//               options={options}
-//               onLeaveFeedback={this.handleFeedback}
-//             />
-//           </ButtonBox>
-//         </Section>
-//         ,
-//         <Section title="Statistics">
-//           <Statistics
-//             good={good}
-//             neutral={neutral}
-//             bad={bad}
-//             total={total}
-//             positivePercentage={positivePercentage}
-//           />
-//         </Section>
-//       </Container>
-//     );
-//   }
-// }
-// export default App;
